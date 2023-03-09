85e164f31b9004e6132b7607bf449b68ab473dc4
tigrazone <tigrazone@gmail.com>
2023-03-09 16:52:30 +0200

    remove unused constructors for Triangle struct

--- src/Shape.hpp
+++ src/Shape.hpp
@@ -47,11 +47,6 @@ struct Triangle {
 	glm::vec2 texcoords[3];
 	uint32_t matid;
 
-	Triangle() = default;
-	Triangle(const glm::vec3 &v0, const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &n0, const glm::vec3 &n1,
-	         const glm::vec3 &n2)
-	    : positions{v0, v1, v2}, normals{n0, n1, n2} {}
-
 	inline AABB GetAABB() const {
 		return {glm::min(positions[0], glm::min(positions[1], positions[2])),
 		        glm::max(positions[0], glm::max(positions[1], positions[2]))};
